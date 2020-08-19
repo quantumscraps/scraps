aac0e7282ca3750461b4c3b34a9b1ec0c745fdb5
Sreehari Sreedev <sreeharisreedev1@gmail.com>
2020-08-18 22:13:24 -0700

    added print/println

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,16 @@ mod bsp;
 mod cpu;
 mod memory;
 mod panic;
+mod print;
 mod util;
 mod drivers;
 mod driver_interfaces;
 
-use core::fmt::Write;
 use driver_interfaces::*;
 
 #[no_mangle]
 pub unsafe extern "C" fn kernel_init() -> ! {
     bsp::UART.init();
-    let _ = writeln!(bsp::UART, "Hello, World!");
+    let _ = println!("[{}] Hello, World!", 0);
     cpu::wait_forever()
 }
---
+++ src/print.rs
@@ -0,0 +1,23 @@
+use crate::bsp;
+#[macro_export]
+macro_rules! print
+{
+	($($args:tt)+) => ({
+			use core::fmt::Write;
+			let _ = write!(bsp::UART, $($args)+);
+	});
+}
+
+#[macro_export]
+macro_rules! println
+{
+	() => ({
+		print!("\r\n")
+	});
+	($fmt:expr) => ({
+		print!(concat!($fmt, "\r\n"))
+	});
+	($fmt:expr, $($args:tt)+) => ({
+		print!(concat!($fmt, "\r\n"), $($args)+)
+	});
+}	
