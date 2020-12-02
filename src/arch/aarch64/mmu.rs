d820fa3fa468436ba095908911f18031f294d640
Sreehari S <sreeharisreedev1@gmail.com>
2020-12-02 15:38:23 -0800

    Skeleton MMU code for aarch64

--- src/arch/aarch64/mmu.rs
+++ src/arch/aarch64/mmu.rs
@@ -1,5 +1,13 @@
+use cortex_a::{barrier, regs::*};
+
 #[naked]
 #[inline(never)]
 pub unsafe fn init() {
-    
+    /*
+    // Initialize the Translation Control Register
+    TCR_EL1.write(TCR_EL1::TG1::KiB_4 // 4 KiB granule size 
+        + TCR_EL1::IPS::Bits_48 // 48 bit addresses (256TiB address space)
+
+    )
+    */
 }
