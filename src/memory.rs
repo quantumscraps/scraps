bfa2e5b5eda3f76ea519ff632e03320d851f1211
Sreehari S <sreeharisreedev1@gmail.com>
2020-12-02 21:58:32 -0800

    Fix bug where we're clearing sizeof(usize) times more bytes of bss

--- src/memory.rs
+++ src/memory.rs
@@ -2,11 +2,18 @@ use crate::link_var;
 link_var!(__bss_start);
 link_var!(__bss_size);
 
+const fn usize_subdivide(size: usize) -> usize {
+    assert!(
+        size % core::mem::size_of::<usize>() == 0,
+        "bss size must be a multiple of sizeof(usize)"
+    );
+    size / core::mem::size_of::<usize>()
+}
 pub unsafe fn setup_environment(dtb_addr: *mut i8) -> ! {
     // get bss section as slice
     let mut slice = core::slice::from_raw_parts_mut(
         &__bss_start as *const _ as *mut usize,
-        &__bss_size as *const _ as usize
+        usize_subdivide(&__bss_size as *const _ as usize)
     );
 
     // zero the slice
