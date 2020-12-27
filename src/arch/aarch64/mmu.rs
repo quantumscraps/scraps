22b6d8234c4e878e6c1f59b81ed8bba27f48dfe1
Sreehari S <sreeharisreedev1@gmail.com>
2020-12-27 15:22:38 -0800

    this doesn't even work

--- src/arch/aarch64/mmu.rs
+++ src/arch/aarch64/mmu.rs
@@ -44,7 +44,7 @@ register_bitfields! {
         ],
         // MemAttr: choose which MAIR index to use
         MemAttr OFFSET(2) NUMBITS(0b11) [],
-        // Is this the end of the translation table walk or not?
+        // Is this a big block or not?
         TYPE OFFSET(1) NUMBITS(0b1) [
             Block = 0b0,
             Table = 0b1
@@ -55,7 +55,58 @@ register_bitfields! {
         ]
     ]
 }
+#[derive(Copy, Clone)]
+#[repr(transparent)]
+struct TableDescriptor(u64);
 
+#[repr(C)]
+#[repr(align(65536))]
+struct TempTranslationTables {
+    l3: [[TableDescriptor; 8192]; 8],
+    l2: [TableDescriptor; 8],
+}
+impl TempTranslationTables {
+    const fn new() -> Self {
+        Self {
+            l3: [[TableDescriptor(0); 8192]; 8],
+            l2: [TableDescriptor(0); 8]
+        }
+    }
+}
+static mut TEMP_TABLES: TempTranslationTables = TempTranslationTables::new();
+trait EzRef {
+    fn get_addr(&self) -> usize;
+}
+impl<T, const N: usize> EzRef for [T; N] {
+    fn get_addr(&self) -> usize {
+        self as *const _ as usize
+    }
+}
+impl convert::From<usize> for TableDescriptor {
+    fn from(addr: usize) -> Self {
+        let temp: InMemoryRegister<u64, TABLE_DESC::Register> = InMemoryRegister::new(0);
+        temp.write(
+            TABLE_DESC::ADDR.val(addr as u64 >> 16)
+            + TABLE_DESC::TYPE::Table
+            + TABLE_DESC::VALID::True
+        );
+        Self(temp.get())
+    }
+}
+impl TableDescriptor {
+    fn new(addr: usize) -> Self {
+        let temp: InMemoryRegister<u64, TABLE_DESC::Register> = InMemoryRegister::new(0);
+        temp.write(
+            TABLE_DESC::ADDR.val(addr as u64 >> 16)
+            + TABLE_DESC::AF::True
+            + TABLE_DESC::TYPE::Table
+            + TABLE_DESC::VALID::True
+            + TABLE_DESC::SH::OuterShareable
+            + TABLE_DESC::MemAttr.val(1)
+        );
+        Self(temp.get())
+    }
+}
 #[inline(never)]
 pub unsafe fn init() {
     /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -71,8 +122,8 @@ pub unsafe fn init() {
             ID_AA64MMFR0_EL1.read(ID_AA64MMFR0_EL1::PARange)
         )
         + TCR_EL1::AS::ASID16Bits  // 16 bit address space ID
-        + TCR_EL1::T0SZ.val(16)    // 48 bit VA in lower half (64-48=16)
-        + TCR_EL1::T1SZ.val(16)    // 48 bit VA in upper half (64-48=16)
+        + TCR_EL1::T0SZ.val(22)    // 42 bit VA in lower half (64-42=22)
+        + TCR_EL1::T1SZ.val(22)    // 42 bit VA in upper half (64-42=22)
         + TCR_EL1::TBI0::Used      // Disable lower half VA tagging for now
         + TCR_EL1::TBI1::Used      // Disable higher half VA tagging for now
         + TCR_EL1::IRGN0::WriteBack_ReadAlloc_WriteAlloc_Cacheable // Write back inner cacheable (lower)
@@ -84,11 +135,39 @@ pub unsafe fn init() {
         + TCR_EL1::SH0::Inner      // Inner shareability lower half
         + TCR_EL1::SH1::Inner      // Inner shareability upper half
     );
-    
+
     // Define 0 as normal cacheable DRAM and 1 as device/mmio
     MAIR_EL1.write(
         MAIR_EL1::Attr0_Normal_Outer::WriteBack_NonTransient_ReadWriteAlloc   // Outer DRAM Cacheability
         + MAIR_EL1::Attr0_Normal_Inner::WriteBack_NonTransient_ReadWriteAlloc // Ditto for inner
         + MAIR_EL1::Attr1_Device::nonGathering_nonReordering_EarlyWriteAck    // MMIO
     );
+
+    // For now just map 4 GiB address space as non cacheable using 512MiB blocks
+    // This will be refactored later
+    /*for i in 0..8 {
+        TEMP_TABLES.l2[0] = (i * 512 * 1024 * 1024 | 0b01 | (1 << 10) | (2 << 8) | (0b001 << 2) | (0 << 7) | (1 << 53));
+    }
+    TTBR0_EL1.set_baddr(TEMP_TABLES.l2.get_addr() as u64);
+    TTBR0_EL1.modify(TTBR0_EL1::CnP::SET);
+    TTBR1_EL1.set_baddr(TEMP_TABLES.l2.get_addr() as u64);
+    TTBR1_EL1.modify(TTBR1_EL1::CnP::SET);
+    */
+    for (i, l2_thing) in TEMP_TABLES.l2.iter_mut().enumerate() {
+        *l2_thing = TEMP_TABLES.l3[i].get_addr().into();
+
+        for (j, l3_entry) in TEMP_TABLES.l3[i].iter_mut().enumerate() {
+            let result = i << 29 + j << 16;
+            *l3_entry = TableDescriptor::new(result);
+        }
+    }
+    TTBR0_EL1.set_baddr(TEMP_TABLES.l2.get_addr() as u64);
+    // Enable MMU
+    barrier::isb(barrier::SY);
+    SCTLR_EL1.modify(
+        SCTLR_EL1::M::Enable // Enable the MMU
+        + SCTLR_EL1::C::Cacheable // Enable data cache
+        + SCTLR_EL1::I::Cacheable // Enable instruction cache
+    );
+    barrier::isb(barrier::SY);
 }
